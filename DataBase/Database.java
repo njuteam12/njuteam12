22a633496a5afa2c4fa5a86abc9d8aed6426d6db
ZJC <jichzhou@outlook.com>
2015-06-09 17:00:28 +0800

    2

--- DataBase/Database.java
+++ DataBase/Database.java
@@ -7,9 +7,9 @@ import java.util.Calendar;
 public class DataBase {
 	
 		private String driverName = "com.microsoft.sqlserver.jdbc.SQLServerDriver"; //加载JDBC驱动
-		private String dbURL = "jdbc:sqlserver://localhost;databaseName=lab1"; //连接服务器和数据库test
-		private String userName = "zxc08ok"; //默认用户名
-		private String userPwd = "12122"; //安装sql server 2005时的密码
+		private String dbURL = "jdbc:sqlserver://localhost;databaseName=master"; //连接服务器和数据库test
+		private String userName = "zjc"; //默认用户名
+		private String userPwd = "123123"; //安装sql server 2005时的密码
 		private Connection dbConn=null;
 		private Statement stmt;
 		public DataBase(){
@@ -36,7 +36,7 @@ public class DataBase {
 		}
 		public boolean addseller(String  name,String psw,String phone,String address,String id,int on) throws SQLException{
 				String str="select id from seller where id='"+id+"'";
-				System.out.println(str);
+				//System.out.println(str);
 				ResultSet rset=stmt.executeQuery(str);
 				try {
 					if(!rset.next()){
@@ -319,17 +319,86 @@ public class DataBase {
 		}
 		if(db!=null){
 			try {
-			db.addseller("Koe","234","2131","asdad","00",0);
-			db.addbuyer("qqq", "qd", "1231", "131231", 0);
-			db.seller_login("Koe", "232");
-			db.buyer_login("qqq", "qd");
-			db.add_menu("0000", "qqwwd", (float)32.2, "qdqwddqd");
-			db.add_order("qdq", "qdqdq", "1210");
-			db.add_comment("0000", "qdqdqwd");
-			db.change_orderstate(0, 1);
-			System.out.println(db.search_menu("dq"));
-			db.changebuyer("qqq", "qwer", "10", null);
-			db.getmenu("qqwwd");
+			db.addseller("黄焖鸡米饭","users","123","南京","Saler0",0);
+			db.addseller("家常小炒","users","123","南京","Saler1",0);
+			db.addseller("饭点社区外卖","users","123","南京","Saler2",0);
+			db.addseller("甜丫丫甜品店","users","123","南京","Saler3",0);
+			db.addseller("咖喱馆","users","123","南京","Saler4",0);
+			db.addseller("木子味中仙","users","123","南京","Saler5",0);
+			db.addseller("仕克德","users","123","南京","Saler6",0);
+			db.addseller("校点点","users","123","南京","Saler7",0);
+			db.addseller("郭家大院","users","123","南京","Saler8",0);
+			//db.addbuyer("qqq", "qd", "1231", "131231", 0);
+			//db.seller_login("Koe", "232");
+			//db.buyer_login("qqq", "qd");
+			db.add_menu("0#0", "黄焖鸡米饭", (float)10.0, "黄焖鸡米饭");
+			db.add_menu("0#1", "黄焖鸡米饭", (float)10.0, "鸡排咖喱饭");
+			db.add_menu("0#2", "黄焖鸡米饭", (float)4.0, "金针菇");
+			db.add_menu("0#3", "黄焖鸡米饭", (float)10.0, "米香鸡块咖喱饭");
+			db.add_menu("0#4", "黄焖鸡米饭", (float)3.0, "千张结");
+			db.add_menu("0#5", "黄焖鸡米饭", (float)2.0, "青菜");
+			
+			db.add_menu("1#0", "家常小炒", (float)17.0, "红烧牛肉饭");
+			db.add_menu("1#1", "家常小炒", (float)20.0, "口水鸡+米饭+汤");
+			db.add_menu("1#2", "家常小炒", (float)15.0, "水煮牛柳+米饭");
+			db.add_menu("1#3", "家常小炒", (float)15.0, "水煮肉片+米饭");
+			db.add_menu("1#4", "家常小炒", (float)7.0, "招牌鸡丝凉面");
+			db.add_menu("1#5", "家常小炒", (float)7.0, "招牌鸡丝凉皮");
+			
+			db.add_menu("2#0", "饭点社区外卖", (float)13.0, "回锅肉套餐饭");
+			db.add_menu("2#1", "饭点社区外卖", (float)22.0, "经典商务套餐");
+			db.add_menu("2#2", "饭点社区外卖", (float)13.0, "秘制鸡腿饭");
+			db.add_menu("2#3", "饭点社区外卖", (float)13.0, "飘香烤鸭饭");
+			db.add_menu("2#4", "饭点社区外卖", (float)13.0, "新奥尔良鸡排饭");
+			db.add_menu("2#5", "饭点社区外卖", (float)13.0, "鸭腿饭");
+			
+			db.add_menu("3#0", "甜丫丫甜品店", (float)24.0, "白雪公主");
+			db.add_menu("3#1", "甜丫丫甜品店", (float)24.0, "白雪小珍珠");
+			db.add_menu("3#2", "甜丫丫甜品店", (float)25.0, "多芒小丸子");
+			db.add_menu("3#3", "甜丫丫甜品店", (float)16.0, "哈密瓜西米捞");
+			db.add_menu("3#4", "甜丫丫甜品店", (float)12.0, "西瓜爆爆珠杯");
+			db.add_menu("3#5", "甜丫丫甜品店", (float)22.0, "杨枝甘露");
+			
+			db.add_menu("4#0", "咖喱馆", (float)17.0, "黄金鲟鱼咖喱饭");
+			db.add_menu("4#1", "咖喱馆", (float)10.0, "鸡米花咖喱饭");
+			db.add_menu("4#2", "咖喱馆", (float)16.0, "咖喱鸡排饭");
+			db.add_menu("4#3", "咖喱馆", (float)10.0, "牛柳咖喱饭");
+			db.add_menu("4#4", "咖喱馆", (float)20.0, "手枪腿饭咖喱饭");
+			db.add_menu("4#5", "咖喱馆", (float)10.0, "鱼排咖喱饭");
+			
+			db.add_menu("5#0", "木子味中仙", (float)9.0, "东坡肉炒饭");
+			db.add_menu("5#1", "木子味中仙", (float)10.0, "宫保鸡丁盖浇饭");
+			db.add_menu("5#2", "木子味中仙", (float)16.0, "韩式酸菜鱼面");
+			db.add_menu("5#3", "木子味中仙", (float)10.0, "里脊肉蛋炒饭");
+			db.add_menu("5#4", "木子味中仙", (float)10.0, "蒜蓉黄瓜");
+			db.add_menu("5#5", "木子味中仙", (float)11.0, "武汉鸡子肉");
+			
+			db.add_menu("6#0", "仕克德", (float)8.0, "超级牛肉堡");
+			db.add_menu("6#1", "仕克德", (float)10.0, "番茄烤肉拌饭");
+			db.add_menu("6#2", "仕克德", (float)16.0, "肥牛咖喱饭");
+			db.add_menu("6#3", "仕克德", (float)16.0, "咖喱烤肉饭");
+			db.add_menu("6#4", "仕克德", (float)15.0, "培根咖喱饭");
+			db.add_menu("6#5", "仕克德", (float)10.0, "香辣烤肉拌饭");
+			
+			db.add_menu("7#0", "校点点", (float)10.0, "风味鱿鱼套餐");
+			db.add_menu("7#1", "校点点", (float)20.0, "干切牛肉套餐饭");
+			db.add_menu("7#2", "校点点", (float)12.0, "杭椒牛柳套餐饭");
+			db.add_menu("7#3", "校点点", (float)21.0, "酸菜鱼");
+			db.add_menu("7#4", "校点点", (float)8.0, "香酥鸡排饭");
+			db.add_menu("7#5", "校点点", (float)8.0, "鱼香肉丝套餐饭");
+			
+			db.add_menu("8#0", "郭家大院", (float)78.0, "大个子龙虾");
+			db.add_menu("8#1", "郭家大院", (float)10.0, "夫妻肺片");
+			db.add_menu("8#2", "郭家大院", (float)10.0, "黑椒牛排饭");
+			db.add_menu("8#3", "郭家大院", (float)3.0, "秘制桂花酸梅汤");
+			db.add_menu("8#4", "郭家大院", (float)10.0, "酸菜卤味饭");
+			db.add_menu("8#5", "郭家大院", (float)10.0, "酸汤肥牛");
+			//db.add_order("qdq", "qdqdq", "1210");
+			//db.add_comment("0000", "qdqdqwd");
+			//db.change_orderstate(0, 1);
+			//System.out.println(db.search_menu("dq"));
+			//db.changebuyer("qqq", "qwer", "10", null);
+			//db.getmenu("qqwwd");
 			//db.changeseller("00", "abc", "qwe", null, null);
 			} catch (SQLException e) {
 				// TODO Auto-generated catch block
